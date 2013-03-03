ab8e73ce73c0c9a1f68326131bdb27cdcb385554
George Mike <g@datermine.com>
2013-03-03 16:59:05 -0500

    Pass the selector to the main function

--- css_transforms.js
+++ css_transforms.js
@@ -20,12 +20,14 @@ function unitless(val) {
  *     margin: 4px;
  *   }
  *
+ * Selector will be: 'div#main'
  * Rule will be: { 'border' : '1px solid #b0bec7', 'margin' : '4px' }
  *
+ * @param selector the full selector text.
  * @param rule a javascript object representing the CSS rule.
  * @return the updated rule.
  */
-function main(rule) {
+function main(selector, rule) {
   return doubleMargin(rule);
 }
 
