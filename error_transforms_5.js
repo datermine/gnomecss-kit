e525c63bfcfc10c38a3c84958dddb96e7c554e30
George Mike <g@datermine.com>
2013-03-03 20:42:35 -0500

    Update errors documentation

--- error_transforms_5.js
+++ error_transforms_5.js
@@ -1,4 +1,4 @@
-/** Condition: Double slash-style comment. */
+/** Condition: Variable does no exist. */
 function main(selector, rule) {
   nonexistent_map["key_does_not_exist"] = 1;
   return rule;
